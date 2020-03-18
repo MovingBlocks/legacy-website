d898660c1b41d4ff1917ceab853ba461bf00dbd9
meetcshah19 <meetcshah19@gmail.com>
2020-03-18 18:41:12 +0530

    Changed variable name

--- js/OSdetect.js
+++ js/OSdetect.js
@@ -12,26 +12,26 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.open("GET", "https://api.github.com/repos/MovingBlocks/TerasologyLauncher/releases/tags/v4.0.0-rc.2", true);
 xmlhttp.send();
 function downloadPackage() {
-    let s = "";
+    let OS = "";
     
     switch (document.getElementById('OS').selectedIndex) {
         case 0:
-            s = "mac";
+            OS = "mac";
             break;
         case 1:
-            s = "mac";
+            OS = "mac";
             break;
         case 2:
-            s = "windows64";
+            OS = "windows64";
             break;
         case 3:
-            s = "windows32";
+            OS = "windows32";
             break;
         case 4:
-            s = "linux64";
+            OS = "linux64";
             break;
         case 5:
-            s = "linux32";
+            OS = "linux32";
             break;
     }
 
@@ -39,7 +39,7 @@ function downloadPackage() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             var data = JSON.parse(xmlhttp.response);
             data.assets.forEach(element => {
-                if (element.name.indexOf(s) != -1) {
+                if (element.name.indexOf(OS) != -1) {
                     window.location = element.browser_download_url;
                 }
             });
